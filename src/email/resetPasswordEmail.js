cc483f525beec5b1d564ccd76343ad71254d2925
nuitfsdev <20521631@gm.uit.edu.vn>
2023-01-07 02:29:50 +0700

    add forgot password

--- src/email/resetPasswordEmail.js
+++ src/email/resetPasswordEmail.js
@@ -19,7 +19,7 @@ exports.resetPasswordEmail=(email,randomCode)=>{
         text: "Quên mật khẩu?", 
         html: `<h2>Booking Hotel xin kính chào quý khách!</h2>
                <p>Để đổi lại mật khẩu cho tài khoản của quý khách. Vùi lòng nhấn vào nút bên dưới để thực hiện đổi mật khẩu!</p>
-                <h1 style="margin-left: 150px;">${randomCode}<h1>
+                <h1 style="margin-left: 150px;">${randomCode}</h1>
                <p>Nếu quý khách không thực hiện yêu cầu này! Xin quý khách vui lòng bỏ qua email này.</p>
                <h3>Cảm ơn quý khách đã tin tưởng và sử dụng dịch vụ của Booking Hotel!</h3>`
         });
